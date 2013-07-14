9a53394dbcb1f54ef69b6e09f6cc65dd23c9be83
arnetheduck@gmail.com <Jacek Sieka>
2013-07-14 11:54:03 +0800

    Add test for long vs pointer ambiguity

--- test/src/se/arnetheduck/j2c/test/methods/AmbiguousOverloads.java
+++ test/src/se/arnetheduck/j2c/test/methods/AmbiguousOverloads.java
@@ -2,14 +2,13 @@ package se.arnetheduck.j2c.test.methods;
 
 public class AmbiguousOverloads {
 	public AmbiguousOverloads(AmbiguousOverloads x) {
-
 	}
 
 	public AmbiguousOverloads(long v) {
-
 	}
 
 	void m() {
 		new AmbiguousOverloads(0); // ptr vs int ambiguity
+		new AmbiguousOverloads(0L); // ptr vs long ambiguity
 	}
 }
