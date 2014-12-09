f99706b012f2ae937c83f3237aa8e01d4c910f10
arnetheduck@gmail.com <Jacek Sieka>
2014-12-09 23:47:47 +0800

    update j2c.cpp to include init_jvm & make_args (issue 12)

--- lib/j2c.cpp
+++ lib/j2c.cpp
@@ -3,35 +3,40 @@
 #include <iostream>
 
 #include <java/lang/Class.hpp>
-#include <java/lang/ClassLoader.hpp>
 
 #include <Array.hpp>
 #include <java/lang/String.hpp>
 
 using namespace java::lang;
 
-void lock(Object *) { }
+void lock(Object*) { }
 
-void unlock(Object *) { }
+void unlock(Object*) { }
 
-String *java::lang::operator "" _j(const char16_t * p, size_t n)
-{
-    char16_tArray *x = new char16_tArray(p, n);
-	String *s = new String();
-	s->value = x;
-	s->count = n;
-    return s->intern(); 
+String *java::lang::operator "" _j(const char16_t* p, size_t n) {
+  auto x = new char16_tArray(p, n);
+  auto s = new String(x, true);
+  return s->intern();
 }
 
-Class *class_(const char16_t *s, int n)
-{
-    return Class::forName(operator "" _j(s, n), false, ClassLoader::getCallerClassLoader());
+Class *class_(const char16_t *s, int n) {
+  return Class::forName(operator "" _j(s, n));
 }
 
 void unimplemented_(const char16_t *name) {
-	std::wcerr << "call to unimplemented: ";
-	// Not quite right but good enough ;)
-	while(*name) std::wcerr << static_cast<wchar_t>(*(name++));
-	std::wcerr << std::endl;
+  std::wcerr << "call to unimplemented: ";
+  // Not quite right but good enough ;)
+  while(*name) std::wcerr << static_cast<wchar_t>(*(name++));
+  std::wcerr << std::endl;
 }
 
+void init_jvm() {
+  // This will be called by the generated main file before running any java code
+  // Use it to initialize system properties and other stuff the JVM should provide
+}
+
+java::lang::StringArray* make_args(int args, char** argv) {
+  // Helper that should convert strings passed to app into
+  // StringArray passed to java main
+  return nullptr;
+}
--- test/src/se/arnetheduck/j2c/test/MainTest.java
+++ test/src/se/arnetheduck/j2c/test/MainTest.java
@@ -1,13 +1,6 @@
 package se.arnetheduck.j2c.test;
 
 public class MainTest {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
 	}
-
 }
